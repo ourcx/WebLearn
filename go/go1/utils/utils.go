c0da91030b351f2078325b078abc3ba4f7b1fa29
小海 <3277975910@qq.com>
2025-04-06 10:13:26 +0800

    更新了一些新的文章

--- go/go1/utils/utils.go
+++ go/go1/utils/utils.go
@@ -1,1 +1,58 @@
 package utils
+
+import (
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+	"time"
+)
+
+func HashPassword(pwd string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(pwd), 12)
+	return string(hash), err
+}
+
+//给密码加密
+
+func GenerateJWT(username string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": username,
+		"exp":      time.Now().Add(time.Hour * 72).Unix(),
+		//把这个时间点转化为时间戳
+	})
+	tokenString, err := token.SignedString([]byte("secret"))
+	//指定相关的设定
+	return tokenString, err
+}
+
+//生成一个jwt，用户令牌
+
+// 对比密码
+func CheckPassword(hash, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func ParseJWT(tokenString string) string {
+	if len(tokenString) > 7 && tokenString[:7] == "Bearer " {
+		tokenString = tokenString[7:]
+	}
+	//先进行粗略的判断和提取
+	//因为一开始拿到的是bearer xxxxx 内容所有要先操纵字符串
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte("secret"), nil
+	})
+	if err != nil {
+		return ""
+	}
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims["username"].(string)
+	}
+	return ""
+}
